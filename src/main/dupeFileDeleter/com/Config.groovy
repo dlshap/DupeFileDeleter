941906ad0104676980b08348b2d9c9685b9b6ba3
David Shapiro <DavidS@dlsa.com>
2016-07-05 14:15:49 -0500

    Updated default root directory

--- src/main/dupeFileDeleter/com/Config.groovy
+++ src/main/dupeFileDeleter/com/Config.groovy
@@ -4,5 +4,6 @@ package main.dupeFileDeleter.com
  * Created by s0041664 on 6/30/2016.
  */
 class Config {
-    def rootDir = "D:\\My Music\\iTunes\\iTunes Media\\Music"
+//    def rootDir = "D:\\My Music\\iTunes\\iTunes Media\\Music"
+    def rootDir = "C:\\test\\DupeFileDeleter"
 }
